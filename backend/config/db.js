63bcc9ead6380ade95da909d6c11297f276beb62
Shashi Kumar <shekhar.shashi735@gmail.com>
2024-07-25 11:49:37 +0530

    MongoDB URL change

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/voosh', {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
